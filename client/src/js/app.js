5f014cd96edf9e975ee1e5728600ef8612a3a93e
Ray Shih <mnf.shih@gmail.com>
2015-12-13 18:43:11 +0800

    setup redux & redux-router

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -1,9 +1,60 @@
 import React from "react";
 import ReactDom from "react-dom";
+
+import {createStore, compose, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
+
+import {Route, Link} from 'react-router';
+import {
+  ReduxRouter,
+  routerStateReducer,
+  reduxReactRouter
+} from 'redux-router';
+import {createHistory} from 'history';
+
 import {Button} from 'react-bootstrap';
 
+// TODO
+const A = () => (
+  <div>
+    A
+  </div>
+);
+const B = () => <div>B</div>;
+
+const App = ({
+  children
+}) => (
+  <div>
+    App
+    <Link to="a">A</Link>
+    <Link to="b">B</Link>
+    {children}
+  </div>
+);
+
+const reducer = combineReducers({
+  router: routerStateReducer
+});
+
+const store = compose(
+  reduxReactRouter({createHistory})
+)(createStore)(reducer);
+
+const Root = () => (
+  <div>
+    <Provider store={store}>
+      <ReduxRouter>
+        <Route path="/" component={App}>
+          <Route path="a" component={A} />
+          <Route path="b" component={B} />
+        </Route>
+      </ReduxRouter>
+    </Provider>
+  </div>
+)
+
 ReactDom.render(
-  <Button bsStyle="primary">Primary</Button>,
-  // <div>Hello React</div>,
-  document.getElementById("container")
+  <Root />,
+  document.getElementById("root")
 );
