3599481f2c532230b772a26369a0eaacff07d1ae
Ray Shih <mnf.shih@gmail.com>
2015-12-13 11:00:15 +0800

    adjust webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,26 @@ var path = require('path');
 var root = path.join(__dirname, 'client');
 
 module.exports = {
-  entry: path.join(root, "./src/js/app.js"),
-  output: {
-    path: path.join(root, "./target/js"),
-    filename: "bundle.js"
+  entry: {
+    javascript: path.join(root, './src/js/app.js'),
+    html: path.join(root, './src/index.html')
   },
+
+  output: {
+    path: path.join(root, './target'),
+    filename: 'bundle.js'
+  },
+
   module: {
     loaders: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.html$/,
+        loader: 'file?name=[name].[ext]'
       }
     ]
   }
