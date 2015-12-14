839acc98e1f181ed98e0a8e866cc9b119fa82d08
Ray Shih <mnf.shih@gmail.com>
2015-12-15 02:03:33 +0800

    add filter

--- client/src/js/components/JogRecords.js
+++ client/src/js/components/JogRecords.js
@@ -45,19 +45,27 @@ const RecordListCell = ({record}) => {
   );
 };
 
-const WeeklyRecords = ({records}) => {
-  const data = _(records).
-    sortBy('date').
-    reverse().
-    value();
+const WeeklyRecords = ({records, filterFrom, filterTo}) => {
+  const filterFromM = filterFrom ? moment(filterFrom) : null;
+  const filterToM = filterTo ? moment(filterTo) : null;
 
-  const lastDate = data[0].date;
+  const lastDate = records[0].date;
 
   const from = moment(lastDate).weekday(0).format('YYYY/MM/DD');
   const to = moment(lastDate).weekday(6).format('YYYY/MM/DD');
 
-  const avePace = (_(data).pluck('pace').sum() / data.length).toFixed(2);
-  const aveSpeed = (_(data).pluck('speed').sum() / data.length).toFixed(2);
+  const filteredData = _(records).
+    filter(r => {
+      if (filterFromM && r.date < filterFromM) return false;
+      if (filterToM && r.date > filterToM) return false;
+      return true;
+    }).
+    sortBy('date').
+    reverse().
+    value();
+
+  const avePace = (_(records).pluck('pace').sum() / records.length).toFixed(2);
+  const aveSpeed = (_(records).pluck('speed').sum() / records.length).toFixed(2);
 
   const style = {
     marginTop: '40px'
@@ -70,7 +78,7 @@ const WeeklyRecords = ({records}) => {
         <span>Average Speed: {aveSpeed} M/s</span>
       </div>
       <h3>{from}~{to}</h3>
-      {data.map(r => <RecordListCell key={r.id} record={r} />)}
+      {filteredData.map(r => <RecordListCell key={r.id} record={r} />)}
     </div>
   );
 };
@@ -79,20 +87,35 @@ const JogRecords = connect(
   (state) => {
     return {jogRecords: state.jogRecords};
   }
-)((props) => {
+)(({jogRecords, filterFrom, filterTo}) => {
+
+  const fromWeek = filterFrom ? moment(filterFrom).week() : null;
+  const toWeek = filterTo ? moment(filterTo).week() : null;
 
   // TODO test it
-  const data = _(props.jogRecords).
-    map(r => Object.assign({}, r, {
-      date: moment(r.date),
-      pace: (r.time / 60) / (r.distance / 1000) ,
-      speed: r.distance / r.time
-    })).
-    groupBy(r => r.date.week()).
-    map(list => ({
-      week: list[0].date.format('YYYY-ww'),
-      records: list
-    })).
+  const data = _(jogRecords).
+    map(r => {
+      const date = moment(r.date);
+      return Object.assign({}, r, {
+        date,
+        pace: (r.time / 60) / (r.distance / 1000) ,
+        speed: r.distance / r.time,
+        weekIndex: date.week()
+      });
+    }).
+    filter(r => {
+      if (fromWeek && r.weekIndex < fromWeek) return false;
+      if (toWeek && r.weekIndex > toWeek) return false;
+      return true;
+    }).
+    groupBy('weekIndex').
+    map(list => {
+      return {
+        weekIndex: list[0].weekIndex,
+        week: list[0].date.format('YYYY-ww'),
+        records: list
+      };
+    }).
     sortBy('week').
     reverse().
     value();
@@ -102,14 +125,13 @@ const JogRecords = connect(
       <WeeklyRecords
         key={recordsByWeek.week}
         records={recordsByWeek.records}
+        filterFrom={filterFrom}
+        filterTo={filterTo}
       />
     ));
 
   return (
     <div>
-      <PageHeader>
-        Your Jogging Records
-      </PageHeader>
       {list}
     </div>
   );
@@ -123,8 +145,55 @@ class JogRecordsListPage extends Component {
     dispatch(fetchAllJobRecords(userId, token));
   }
 
+  state = {}
+
+  filterFromChanged = () => {
+    this.setState({
+      filterFrom: this.refs.filterFrom.value
+    });
+  }
+
+  filterToChanged = () => {
+    this.setState({
+      filterTo: this.refs.filterTo.value
+    });
+  }
+
   render() {
-    return <JogRecords />;
+    const filterWrapperStyle = {
+      top: '15px',
+      position: 'relative'
+    };
+
+    return (
+      <div>
+        <div className="pull-right" style={filterWrapperStyle}>
+          <form className="form-inline" onSubmit={(e) => e.preventDefault()}>
+            Filter:&nbsp;
+            <input
+              className="form-control"
+              type="date"
+              ref="filterFrom"
+              onChange={this.filterFromChanged}
+            />
+            &nbsp;~&nbsp;
+            <input
+              className="form-control"
+              type="date"
+              ref="filterTo"
+              onChange={this.filterToChanged}
+            />
+          </form>
+        </div>
+        <PageHeader>
+          Your Jogging Records
+        </PageHeader>
+        <JogRecords
+          filterFrom={this.state.filterFrom}
+          filterTo={this.state.filterTo}
+        />
+      </div>
+    );
   }
 
 };
