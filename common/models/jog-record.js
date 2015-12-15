34f6eb605daf6a7668e32c5c2ec80ed55d8ecf93
Ray Shih <mnf.shih@gmail.com>
2015-12-15 23:36:41 +0800

    remove unused hook

--- common/models/jog-record.js
+++ common/models/jog-record.js
@@ -1,7 +1,2 @@
 module.exports = function(JogRecord) {
-  JogRecord.beforeRemote('create', function(context, user, next) {
-    var req = context.req;
-    req.body.userId = req.accessToken.userId;
-    next();
-  });
 };
