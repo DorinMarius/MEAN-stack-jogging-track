36052ad66f6ae325f7974d1978d5c72eb3f832e2
Ray Shih <mnf.shih@gmail.com>
2015-12-15 22:29:50 +0800

    adjust db config, make more test data

--- server/boot/create-sample-models.js
+++ server/boot/create-sample-models.js
@@ -8,7 +8,7 @@ module.exports = function (app) {
     var Model = app.models[modelName];
 
     return function (data) {
-      console.log('create ' + modelName + ': ' + JSON.stringify(data, null, 2));
+      // console.log('create ' + modelName + ': ' + JSON.stringify(data, null, 2));
       return new Promise(function (resolve, reject) {
         mongoDB.automigrate(modelName, function (err) {
           if (err) return reject(err);
@@ -25,7 +25,17 @@ module.exports = function (app) {
   var createRecords = createGenerator('JogRecord');
 
   var users = [
-    {username: 'JogTal Admin', email: 'admin@jogtal.com', password: 'qwer1234'}
+    {username: 'Ray', email: 'ray@gmail.com', password: 'qwer1234'},
+    {username: 'Iris', email: 'iris@yahoo.com', password: 'asdfqwer'},
+    {username: 'Angela', email: 'angela@hotmail.com', password: 'zxcvasdf'}
+  ];
+
+  var managers = [
+    {username: 'JogTal Manager', email: 'manager@jogtal.com', password: '1234qwer'}
+  ];
+
+  var admins = [
+    {username: 'Jogtal Admin', email: 'admin@jogtal.com', password: 'asdfqwer'}
   ];
 
   var DAY_IN_MILLISECONDS = 1000 * 60 * 60 * 24;
@@ -33,22 +43,27 @@ module.exports = function (app) {
     return Math.floor((Math.random() * n));
   };
 
-  createUsers(users).
-    then(function (users) {
-      var days = 10 + randInt(10);
-      var records = _.range(days).map(function () {
+  createUsers(
+    users.
+    concat(managers).
+    concat(admins)
+  ).then(function (users) {
+    var records = _(users).map(function (user) {
+      var days = 5 + randInt(10);
+      return _.range(days).map(function () {
         return {
           date: Date.now() - DAY_IN_MILLISECONDS * randInt(30),
           distance: randInt(15000),
           time: randInt(2 * 60 * 60),
-          userId: users[0].id
-        }
+          userId: user.id
+        };
       });
+    }).flatten().value();
 
-      return createRecords(records);
-    }).
-    catch(console.error).
-    done(function () {
-      console.log('done');
-    });
+    return createRecords(records);
+  }).
+  catch(console.error).
+  done(function () {
+    console.log('done prepare sample data');
+  });
 };
